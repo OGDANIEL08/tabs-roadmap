ce8b04e7338e3618c8adeffc5830499e89b718cb
OGDANIEL08 <ogundairodaniel06@gmail.com>
2026-04-13 13:49:33 +0100

    roadmap.sh tabs

---
+++ script.js
@@ -0,0 +1,16 @@
+const buttons = document.querySelectorAll('.tab-btn')
+const contents = document.querySelectorAll('.tab-content')
+
+buttons.forEach((button) => {
+  button.addEventListener('click', () => {
+    //Remove active class from all
+    buttons.forEach(btn => btn.classList.remove('active') )
+    contents.forEach(content => content.classList.remove('active') )
+
+    // Add active class to clicked tab
+    button.classList.add('active');
+
+    const tabId = button.getAttribute("data-tab");
+    document.getElementById(tabId).classList.add("active");
+  })
+})
